28ae0f521c71307f101af7f48efc8ef62f9c14fd
kanae nakaji <nae6p21@icloud.com>
2025-11-25 15:44:51 +0900

    add files

---
+++ src/resources/views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <h1>ブラウザに画面を表示できた！</h1>
+    <p><?php echo $content ?></p>
+    <table>
+        <tr>
+            <th>パスパラメータ</th>
+            <td><?php echo $param."=おはようございます" ?></td>
+        </tr>
+    </table>
+</body>
+</html>
